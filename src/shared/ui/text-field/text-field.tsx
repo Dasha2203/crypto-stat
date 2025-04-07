4ceaf417289e610833a977e4f253d209a33b4899
Darya Korbut <kitkatlolipop@mail.ru>
2025-04-07 13:06:12 +0300

    fix: add text ellipses for text-field component

--- src/shared/ui/text-field/text-field.tsx
+++ src/shared/ui/text-field/text-field.tsx
@@ -1,6 +1,6 @@
+import clsx from "clsx";
 import { TextFieldProps } from "./text-field.types";
 import styles from "./styles.module.scss";
-import clsx from "clsx";
 
 export const TextField = ({
   error,
@@ -8,6 +8,7 @@ export const TextField = ({
   label,
   color = "primary",
   className,
+  style,
   ...props
 }: TextFieldProps) => {
   return (
@@ -16,15 +17,13 @@ export const TextField = ({
         styles["text-field"],
         styles[`text-field_${color}`],
         error && styles["error"],
+        className,
       )}
+      style={style}
     >
       {label && <span className={styles["text-field__label"]}>{label}</span>}
-      <div>
-        <input
-          className={clsx(styles["text-field__input"], className)}
-          {...props}
-        />
-      </div>
+
+      <input className={clsx(styles["text-field__input"])} {...props} />
       {helperText && (
         <span className={styles["text-field__helper"]}>{helperText}</span>
       )}
