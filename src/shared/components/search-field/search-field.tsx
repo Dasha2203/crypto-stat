6318ec11af853f0a8ddb74ce5e9552c38118418a
Darya Korbut <kitkatlolipop@mail.ru>
2025-04-07 13:04:58 +0300

    feat: add search field component and useUpdateSearchParams hook

---
+++ src/shared/components/search-field/search-field.tsx
@@ -0,0 +1,46 @@
+import { Controller, useForm } from "react-hook-form";
+import { useUpdateSearchParams } from "@shared/hooks";
+import { Button, TextField } from "@shared/ui";
+import { SearchFieldProps, SearchFormData } from "./search-field.types";
+import styles from "./styles.module.scss";
+
+const SearchField = ({ placeHolder }: SearchFieldProps) => {
+  const { updateSearchParams } = useUpdateSearchParams();
+  const { control, handleSubmit } = useForm<SearchFormData>({
+    defaultValues: {
+      search: "",
+    },
+  });
+
+  const onSubmit = ({ search }: SearchFormData) => {
+    updateSearchParams("query", search);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles["form"]}>
+      <Controller
+        name="search"
+        control={control}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            placeholder={placeHolder}
+            style={{ width: "100%", maxWidth: "none" }}
+            className={styles["input"]}
+          />
+        )}
+      />
+
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        className={styles["form__button"]}
+      >
+        Search
+      </Button>
+    </form>
+  );
+};
+
+export default SearchField;
