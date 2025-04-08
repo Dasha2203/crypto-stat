ee46443f8a9adeb752bc1bb3a96c3ceba72db530
Darya Korbut <kitkatlolipop@mail.ru>
2025-04-08 12:42:35 +0300

    fix: proxy on vercal

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api/proxy",
+  baseURL: "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
   },
 });
